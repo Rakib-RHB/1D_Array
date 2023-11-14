1868f3f4abe80e86911b78a6f9ad8056905f6f65
Rakib Hasan <140387583+Rakib-RHB@users.noreply.github.com>
2023-11-14 22:24:17 +0600

    Add files via upload

---
+++ Max & Mini.c
@@ -0,0 +1,31 @@
+/* Take N students marks
+as input and store them
+in an array. Find
+highest/lowest mark */
+
+#include<stdio.h>
+int main()
+{
+    int n,i,x;
+    printf("How Many Number : ");
+    scanf("%d",&n);
+    int A[n];
+    for(i = 0;i<n;i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    int max = A[0];
+    for(i=0;i<n;i++)
+    {
+        if(max < A[i])
+        {
+            max = A[i];
+        }
+
+    }
+    printf("Maximum : %d",max);
+
+
+
+
+}
